93d5dd87d31d505bb2ed7931340fe99259ffda02
SantinoPinnola <83730569+SantinoPinnola@users.noreply.github.com>
2021-10-28 21:57:08 -0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,2 @@
+artillery quick --count 50 -n 20 "http://localhost:8080/api/infoWithConsoleLog" > result_bloq.txt
+artillery quick --count 50 -n 20 "http://localhost:8080/api/infoWithoutConsoleLog" > result_nobloq.txt
